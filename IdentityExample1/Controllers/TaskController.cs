0f9167a4f3b6888ce8562967d4bde3870666f98f
Schwartz2 (Secondary) <MSchwartz2-@quickenloans.com>
2020-04-16 11:51:26 -0400

    first commit

---
+++ IdentityExample1/Controllers/TaskController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityExample1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace IdentityExample1.Controllers
+{
+    public class TaskController : Controller
+    {
+        IConfiguration ConfigRoot;
+        private DAL dal;
+        public TaskController(IConfiguration config)
+        {
+            ConfigRoot = config;
+            dal = new DAL(config.GetConnectionString("default"));
+        }
+
+        public IActionResult Index()
+        {
+            var results = dal.GetAllTasks();
+
+            ViewData["Tasks"] = results;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddTask()
+        {
+            return View(new UserTasks());
+        }
+
+        [HttpPost]
+        public IActionResult AddTask(UserTasks t)
+        {
+            int result = dal.CreateTask(t);
+
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ IdentityExample1/Models/DAL.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityExample1.Models
+{
+    public class DAL
+    {
+        private SqlConnection conn;
+
+        public DAL(string connectionString)
+        {
+            conn = new SqlConnection(connectionString);
+        }
+         
+        public IEnumerable<UserTasks> GetAllTasks()
+        {
+            string queryString = "SELECT * FROM UserTasks";
+
+            return conn.Query<UserTasks>(queryString);
+
+        }
+
+        public int CreateTask(UserTasks t)
+        {
+            t.Complete = 1; //always create status=1
+
+            string addQuery = "INSERT INTO UserTasks (OwnerId, Description, DueDate, Complete) ";
+            addQuery += "VALUES (@OwnerId, @Description, @DueDate, @Complete)";
+
+            return conn.Execute(addQuery, t);
+        }
+    }
+}
